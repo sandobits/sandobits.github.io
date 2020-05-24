4d5c95679fc143b97fd73b716a04884c8c2c0f80
sandobits <sandersonbarbosa36@gmail.com>
2020-05-23 23:16:27 -0300

    build

---
+++ precache-manifest.f0442e8afe846e81ffce7b0588270c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0df4ca6f64162d508dc4035b574af83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4a0b1d64fbf33f8a97e",
+    "url": "/static/css/main.fa30880a.chunk.css"
+  },
+  {
+    "revision": "bc417c1df6936b9dc89b",
+    "url": "/static/js/2.4b7f2351.chunk.js"
+  },
+  {
+    "revision": "e23d72e353070f497294c1592aa53554",
+    "url": "/static/js/2.4b7f2351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4a0b1d64fbf33f8a97e",
+    "url": "/static/js/main.96cb5b14.chunk.js"
+  },
+  {
+    "revision": "23923ae106908060ad06",
+    "url": "/static/js/runtime-main.1c8c8342.js"
+  }
+]);
